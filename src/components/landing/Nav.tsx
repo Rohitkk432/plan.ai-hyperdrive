7b864b976fdbf6fd99f1f9cf9cfab8ce088fbde4
Rohitkk432 <rohitkodam4@gmail.com>
2023-10-23 21:41:31 +0530

    responsiveness fix nav

--- src/components/landing/Nav.tsx
+++ src/components/landing/Nav.tsx
@@ -10,8 +10,8 @@ interface NavProps {
 
 export const Nav: React.FC<NavProps> = ({ handleFeatureClick }) => {
     return (
-        <div className="mt-5 inline-flex w-full items-center justify-between px-[3rem]">
-            <AnchorLink href={"#"} className="h-11 xl:h-12 3xl:h-14">
+        <div className="mt-5 inline-flex w-full items-center justify-between px-[1rem] sm:px-[3rem]">
+            <AnchorLink href={"#"} className="h-9 xs:h-10 md:h-11 xl:h-12 3xl:h-14">
                 <Image
                     alt="image"
                     width={0}
@@ -21,7 +21,7 @@ export const Nav: React.FC<NavProps> = ({ handleFeatureClick }) => {
                     src={LogoFull}
                 />
             </AnchorLink>
-            <div className="mr-10 md:mr-20 flex items-center justify-center gap-4 md:gap-7 text-xs md:text-sm font-normal whitespace-pre tracking-tight text-white xl:text-base 3xl:text-lg">
+            <div className="mr-0 sm:mr-10 md:mr-20 flex items-center justify-center gap-3 sm:gap-5 md:gap-7 text-xs md:text-sm font-normal whitespace-pre tracking-tight text-white xl:text-base 3xl:text-lg">
                 <div onClick={handleFeatureClick} className="cursor-pointer">
                     features
                 </div>
@@ -37,7 +37,7 @@ export const Nav: React.FC<NavProps> = ({ handleFeatureClick }) => {
             </div>
             <AnchorLink
                 href={"mailto:hellodefiOS@gmail.com"}
-                className="flex items-center justify-center gap-4 text-xs md:text-sm font-medium whitespace-pre tracking-tight text-primary xl:text-base 3xl:text-lg">
+                className="flex items-center justify-center text-xs md:text-sm font-medium whitespace-pre tracking-tight text-primary xl:text-base 3xl:text-lg">
                 <div>say hi!</div>
             </AnchorLink>
         </div>
